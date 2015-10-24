5468a0d98a4f91edebb6993c8553550fb1794b37
khp <kanghee.p@gmail.com>
2015-10-24 15:18:27 -0700

    changed feedback columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024214226) do
+ActiveRecord::Schema.define(version: 20151024221253) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
-    t.text     "content"
+    t.string   "contact"
+    t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category"
   end
 
   create_table "hours", force: :cascade do |t|
